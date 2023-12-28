58787cb99fb97c08465d6e803b6c7d7d95a50538
Subhan Azam <subhanshabeer2005@gmail.com>
2023-12-28 20:03:17 +0500

    initial commit

--- app/(components)/contactMe/contactMe.js
+++ app/(components)/contactMe/contactMe.js
@@ -1,3 +1,79 @@
+// "use client";
+// import HeaderBtn from "../headerBtn/headerBtn";
+// import "./contactMe.css";
+// import { collection, addDoc } from "firebase/firestore";
+// import { db } from "@/config/firebase";
+// import { useState } from "react";
+
+// export default function ContactMe() {
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [subject, setSubject] = useState("");
+//   const [msg, setMsg] = useState("");
+
+//   const onClickHandler = async () => {
+//     let userData = {
+//       name,
+//       email,
+//       subject,
+//       msg,
+//     };
+
+//     try {
+//       const collectionName = collection(db, "userInfo");
+//       await addDoc(collectionName, userData);
+//       console.log("Document written with ID: ");
+//     } catch (e) {
+//       console.error("Error adding document: ", e);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="contact_inputs">
+//         <form
+//           onSubmit={onClickHandler}
+//           action="mailto:youremail@example.com"
+//           method="post"
+//           enctype="text/plain"
+//         >
+//           <input
+//             autoComplete="off"
+//             onChange={(e) => setName(e.target.value)}
+//             placeholder="Your Name*"
+//             type="text"
+//             required
+//           />
+
+//           <input
+//             autoComplete="off"
+//             onChange={(e) => setEmail(e.target.value)}
+//             placeholder="Your Email*"
+//             type="email"
+//             required
+//           />
+
+//           <input
+//             autoComplete="off"
+//             onChange={(e) => setSubject(e.target.value)}
+//             placeholder="Subject"
+//             type="text"
+//             required
+//           />
+
+//           <textarea
+//             onChange={(e) => setMsg(e.target.value)}
+//             placeholder="Message"
+//             required
+//           ></textarea>
+
+//           <HeaderBtn type="submit" title="Submit" />
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
 "use client";
 import HeaderBtn from "../headerBtn/headerBtn";
 import "./contactMe.css";
@@ -11,7 +87,9 @@ export default function ContactMe() {
   const [subject, setSubject] = useState("");
   const [msg, setMsg] = useState("");
 
-  const onClickHandler = async () => {
+  const onClickHandler = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+
     let userData = {
       name,
       email,
@@ -20,127 +98,50 @@ export default function ContactMe() {
     };
 
     try {
-      const collectionName = collection(db, "userInfo");
-      await addDoc(collectionName, userData);
-      console.log("Document written with ID: ");
-    } catch (e) {
-      console.error("Error adding document: ", e);
+      const collectionRef = collection(db, "userInfo"); // Use collection reference
+      await addDoc(collectionRef, userData); // Add document to the collection
+      console.log("Document written");
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
   };
 
   return (
-    <>
-      <div className="contact_inputs">
-        <form
-          onSubmit={onClickHandler}
-          action="mailto:youremail@example.com"
-          method="post"
-          enctype="text/plain"
-        >
-          <input
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Your Name*"
-            type="text"
-            required
-          />
+    <div className="contact_inputs">
+      <form onSubmit={onClickHandler}>
+        <input
+          autoComplete="off"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Your Name*"
+          type="text"
+          required
+        />
 
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Your Email*"
-            type="email"
-            required
-          />
+        <input
+          autoComplete="off"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Your Email*"
+          type="email"
+          required
+        />
 
-          <input
-            onChange={(e) => setSubject(e.target.value)}
-            placeholder="Subject"
-            type="text"
-            required
-          />
+        <input
+          autoComplete="off"
+          onChange={(e) => setSubject(e.target.value)}
+          placeholder="Subject"
+          type="text"
+          required
+        />
 
-          <textarea
-            onChange={(e) => setMsg(e.target.value)}
-            placeholder="Message"
-            required
-          ></textarea>
+        <textarea
+          autoComplete="off"
+          onChange={(e) => setMsg(e.target.value)}
+          placeholder="Message"
+          required
+        ></textarea>
 
-          <HeaderBtn type="submit" title="Submit" />
-        </form>
-      </div>
-    </>
+        <HeaderBtn type="submit" title="Submit" />
+      </form>
+    </div>
   );
 }
-
-
-
-
-// "use client"
-// import HeaderBtn from "../headerBtn/headerBtn";
-// import "./contactMe.css";
-// import { collection, addDoc } from "firebase/firestore";
-// import { db } from "@/config/firebase";
-// import { useState } from "react";
-
-// export default function ContactMe() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [subject, setSubject] = useState("");
-//   const [msg, setMsg] = useState("");
-
-//   const onClickHandler = async (e) => {
-//     e.preventDefault(); // Prevent default form submission
-
-//     let userData = {
-//       name,
-//       email,
-//       subject,
-//       msg,
-//     };
-
-//     try {
-//       const collectionRef = collection(db, "userInfo"); // Use collection reference
-//       await addDoc(collectionRef, userData); // Add document to the collection
-//       console.log("Document written");
-//     } catch (error) {
-//       console.error("Error adding document: ", error);
-//     }
-//   };
-
-//   return (
-//     <div className="contact_inputs">
-//       <form onSubmit={onClickHandler}>
-//         <input
-//           onChange={(e) => setName(e.target.value)}
-//           placeholder="Your Name*"
-//           type="text"
-//           required
-//         />
-
-//         <input
-//           onChange={(e) => setEmail(e.target.value)}
-//           placeholder="Your Email*"
-//           type="email"
-//           required
-//         />
-
-//         <input
-//           onChange={(e) => setSubject(e.target.value)}
-//           placeholder="Subject"
-//           type="text"
-//           required
-//         />
-
-//         <textarea
-//           onChange={(e) => setMsg(e.target.value)}
-//           placeholder="Message"
-//           required
-//         ></textarea>
-
-//         <HeaderBtn
-//           type="submit"
-//           title="Submit"
-//         />
-//       </form>
-//     </div>
-//   );
-// }
